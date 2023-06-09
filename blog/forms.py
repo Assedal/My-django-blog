b24ab889bdcfc94c27e0a89f6f1f6aa44beac629
Assedal <96593122+Assedal@users.noreply.github.com>
2023-06-09 13:05:23 +0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 from .models import Post
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ( "username", "email" )
+
 class PostForm(forms.ModelForm):
 
     class Meta:
